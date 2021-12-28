6e329e559441d1f297f49c6b920cb55aa5a7fb45
melvyniandrag <melvyniandrag@gmail.com>
2021-12-28 01:31:19 -0500

    revision to add number > letter conversion

--- compute_averages.py
+++ compute_averages.py
@@ -4,7 +4,42 @@ import pandas as pd
 import sys
 
 NUM_GRADES_TO_DROP = 2
+HW_WEIGHT = 0.7
+EXAM_WEIGHT = 0.3
 
+A_CUTOFF =       4.0 * 25
+A_MINUS_CUTOFF = 3.7 * 25
+B_PLUS_CUTOFF =  3.3 * 25
+B_CUTOFF =       3.0 * 25
+B_MINUS_CUTOFF = 2.7 * 25
+C_PLUS_CUTOFF =  2.3 * 25
+C_CUTOFF =       2.0 * 25
+C_MINUS_CUTOFF = 1.7 * 25
+D_CUTOFF =       1.0 * 25
+F_CUTOFF =       0
+
+
+def number_to_letter(grade_as_number):
+    if(grade_as_number >= A_CUTOFF):
+        return "A"
+    elif (grade_as_number >= A_MINUS_CUTOFF):
+        return "A-"
+    elif (grade_as_number >= B_PLUS_CUTOFF):
+        return "B+"
+    elif (grade_as_number >= B_CUTOFF):
+        return "B"
+    elif (grade_as_number >= B_MINUS_CUTOFF):
+        return "B-"
+    elif (grade_as_number >= C_PLUS_CUTOFF):
+        return "C+"
+    elif (grade_as_number >= C_CUTOFF):
+        return "C"
+    elif (grade_as_number >= C_MINUS_CUTOFF):
+        return "C-"
+    elif (grade_as_number >= D_CUTOFF):
+        return "D"
+    else:
+        return "F"
 
 grades = pd.read_csv(sys.argv[1])
 print("#" * 80)
@@ -52,7 +87,6 @@ print("#" * 80)
 
 print("#" * 80)
 print("homework values after 0.70 weighting:")
-HW_WEIGHT = 0.7
 hw_points = hw_average.apply(lambda x: x*HW_WEIGHT)
 print(hw_points)
 print("#" * 80)
@@ -74,14 +108,27 @@ print("#" * 80)
 
 print("#" * 80)
 print("exam values after 0.3 weighting:")
-EXAM_WEIGHT = 0.3
 exam_points = exam_avg.apply(lambda x: x* EXAM_WEIGHT)
 print(exam_points)
 print("#" * 80)
 
 print("#" * 80)
+print("Generating final scores...")
+final_number = hw_points + exam_points
+print(final_number)
+print("#" * 80)
+
+print("#" * 80)
+print("Converting to letter...")
+letter = final_number.apply(lambda x: number_to_letter(x))
+print(letter)
+print("#" * 80)
+
+
+
+print("#" * 80)
 print("Final scores:")
-final = pd.concat([grades["last name"], hw_points, exam_points, hw_points+exam_points], axis=1, keys=["last name", "hw", "exam", "final"])
+final = pd.concat([grades["last name"], letter, final_number, hw_points, exam_points], axis=1, keys=["last name", "letter", "final number", "hw", "exam"])
 print(final)
 print(type(final))
 print("#" * 80)
